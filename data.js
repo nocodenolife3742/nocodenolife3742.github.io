f448196bffaa2874d5d1980a6ad24702e3d14ba0
Eric <ericncnl3742@gmail.com>
2025-05-05 22:16:38 +0800

    Modify Descriptions

--- data.js
+++ data.js
@@ -4,14 +4,14 @@ const projectsData = [
         title: '洛克人 (Mega Man) 遊戲重製',
         githubLink: 'https://github.com/kesshoban3310/OOPL2024S',
         description: '以物件導向程式設計 (OOP) 為核心，開發可模組化、可重複使用的遊戲組件，忠實還原經典洛克人（Mega Man）遊戲體驗',
-        technologies: ['C++', 'Cmake', 'OOP', 'Git']
+        technologies: ['C++', 'Cmake', 'OOP Design', 'Game Development', 'Git']
     },
     {
         image: 'images/e-commerce.png',
         title: '電子商務網站',
         githubLink: 'https://github.com/NTUT-Database-System-Course/ACW',
         description: '使用 React 和 Tailwind CSS 設計前端介面，且後端採用 Golang 和 PostgreSQL，透過 Swagger 定義 API，方便開發者擴展功能或整合至其他系統',
-        technologies: ['Golang', 'PostgreSQL', 'React', 'Tailwind CSS', 'Swagger API', 'Frontend', 'Backend']
+        technologies: ['Golang', 'PostgreSQL', 'React', 'Tailwind CSS', 'Swagger API', 'Frontend', 'Backend', 'Docker']
     },
     {
         image: 'images/lisp_compiler.png',
@@ -25,7 +25,7 @@ const projectsData = [
         title: '俄羅斯方塊對戰遊戲',
         githubLink: 'https://github.com/nocodenolife3742/Tetris-Battle',
         description: '開發了一款支援即時對戰的雙人連線俄羅斯方塊遊戲，後端使用 Express.js，並透過 Socket.IO 實現即時通訊，使玩家能夠與朋友進行對戰',
-        technologies: ['JavaScript', 'Node.js', 'Express.js', 'Socket.IO', 'HTML5', 'CSS3']
+        technologies: ['JavaScript', 'Node.js', 'Express.js', 'Socket.IO', 'HTML', 'CSS']
     }
 
 ];
